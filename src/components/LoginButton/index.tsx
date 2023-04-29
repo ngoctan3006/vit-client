e20cf3db653a14d3e99ecb4a79d9caf8fa7dd81c
Tan <>
2023-04-29 15:24:52 +0700

    update ui

--- src/components/LoginButton/index.tsx
+++ src/components/LoginButton/index.tsx
@@ -3,15 +3,16 @@ import './index.scss';
 
 interface Props {
   children?: React.ReactNode;
-  onClick: React.MouseEventHandler<HTMLButtonElement> | undefined;
+  onClick?: React.MouseEventHandler<HTMLButtonElement> | undefined;
+  type?: 'button' | 'submit' | 'reset' | undefined;
 }
 
-const LogginButton: React.FC<Props> = ({ children, onClick }) => {
+const LogginButton: React.FC<Props> = ({ children, onClick, type }) => {
   return (
     <div className="login-button-wrap">
       <div className="login-btn-wrap">
         <div className="login-bg"></div>
-        <button className="login-btn" onClick={onClick}>
+        <button className="login-btn" type={type} onClick={onClick}>
           {children}
         </button>
       </div>
--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,43 +1,73 @@
-import { Button, Input } from 'antd';
+import { Button, Form, Input } from 'antd';
 import React from 'react';
 import { BiLockAlt } from 'react-icons/bi';
 import { FaRegUser } from 'react-icons/fa';
-import './login.scss';
 import LogginButton from '../../components/LoginButton';
+import './login.scss';
+
+interface LoginState {
+  username: string;
+  password: string;
+}
 
 const LoginPage: React.FC = () => {
+  const [form] = Form.useForm();
+  const handleLogin = async (data: LoginState) => {
+    console.log(data);
+
+    form.resetFields();
+  };
+
   return (
     <div className="login-container">
       <div className="wrap">
-        <form>
+        <Form form={form} onFinish={handleLogin}>
           <h3 className="title text-center">Đăng nhập</h3>
           <div className="group">
             <label className="label" htmlFor="username">
               Tên đăng nhập
             </label>
-            <Input
-              size="large"
-              placeholder="Tên đăng nhập"
-              prefix={<FaRegUser />}
-            />
+            <Form.Item
+              name="username"
+              rules={[
+                { required: true, message: 'Vui lòng nhập tên đăng nhập' },
+              ]}
+              hasFeedback
+            >
+              <Input
+                id="username"
+                size="large"
+                placeholder="Tên đăng nhập"
+                prefix={<FaRegUser />}
+              />
+            </Form.Item>
           </div>
           <div className="group">
             <label className="label" htmlFor="password">
               Mật khẩu
             </label>
-            <Input.Password
-              size="large"
-              placeholder="Mật khẩu"
-              prefix={<BiLockAlt />}
-            />
+            <Form.Item
+              name="password"
+              rules={[
+                { required: true, message: 'Vui lòng nhập mật khẩu' },
+                { min: 6, message: 'Mật khẩu phải có ít nhất 6 ký tự' },
+                { max: 32, message: 'Mật khẩu không được quá 32 ký tự' },
+              ]}
+              hasFeedback
+            >
+              <Input.Password
+                id="password"
+                size="large"
+                placeholder="Mật khẩu"
+                prefix={<BiLockAlt />}
+              />
+            </Form.Item>
           </div>
           <div className="text-right">
             <Button type="text">Quên mật khẩu?</Button>
           </div>
-          <LogginButton onClick={() => console.log('hello world')}>
-            Đăng nhập
-          </LogginButton>
-        </form>
+          <LogginButton type="submit">Đăng nhập</LogginButton>
+        </Form>
       </div>
     </div>
   );
