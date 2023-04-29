13ba6fe466c078784aefd70fe8a32e5f5550c22f
Tan <>
2023-04-29 15:29:07 +0700

    remove react-toastify

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,10 @@
 import React from 'react';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 import Router from './router/Router';
 
 const App: React.FC = () => {
   return (
     <>
       <Router />
-      <ToastContainer
-        position="top-right"
-        autoClose={3000}
-        limit={5}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light"
-      />
     </>
   );
 };
