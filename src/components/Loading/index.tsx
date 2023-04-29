a6476116e0197dea10cdb4def31ec2386f56e7bf
Tan <>
2023-04-30 00:40:44 +0700

    add loading component

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Lottie from 'react-lottie';
+import animationData from '../../assets/lotties/loading.json';
+import './index.scss';
+
+const Loading: React.FC = () => {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  };
+
+  return (
+    <div className="loading-container">
+      <Lottie options={defaultOptions} height={100} width={100} />
+    </div>
+  );
+};
+
+export default Loading;
