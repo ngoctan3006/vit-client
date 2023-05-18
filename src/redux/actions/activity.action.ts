a15e860f3e1aeb6b6a531f9a90fa75a9271a79ed
Tan <>
2023-05-18 14:56:54 +0700

    get activity deleted api

--- src/redux/actions/activity.action.ts
+++ src/redux/actions/activity.action.ts
@@ -1,8 +1,9 @@
-import { API } from '../../services/axios';
-import { QueryParamType, defaultQueryParam } from './../../constants/type';
 import { createAsyncThunk } from '@reduxjs/toolkit';
+import { API } from '../../services/axios';
+import { Activity } from '../slices/activity.slice';
+import { QueryParamType, defaultQueryParam } from './../../constants/type';
 
-export const getAllActivity = createAsyncThunk(
+export const getAllActivity = createAsyncThunk<Activity[], QueryParamType>(
   'activity/getAll',
   async (
     { page, limit }: QueryParamType = defaultQueryParam,
@@ -12,6 +13,26 @@ export const getAllActivity = createAsyncThunk(
       const {
         data: { data: res },
       } = await API.get(`activity?page=${page}&limit=${limit}`);
+      return res;
+    } catch (error: any) {
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+
+export const getAllActivityDeleted = createAsyncThunk<
+  Activity[],
+  QueryParamType
+>(
+  'activity/getAllDeleted',
+  async (
+    { page, limit }: QueryParamType = defaultQueryParam,
+    { rejectWithValue }
+  ) => {
+    try {
+      const {
+        data: { data: res },
+      } = await API.get(`activity/trash?page=${page}&limit=${limit}`);
       return res;
     } catch (error: any) {
       return rejectWithValue(error.response.data);
