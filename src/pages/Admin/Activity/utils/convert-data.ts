bdd731a342f955915a155481215ab1d10c361e65
Tan <ngoctana1k2014@gmail.com>
2023-07-22 05:46:05 +0700

    convert data

--- src/pages/Admin/Activity/utils/convert-data.ts
+++ src/pages/Admin/Activity/utils/convert-data.ts
@@ -1,9 +1,11 @@
 import { GetActivityMember } from 'src/redux/actions';
-import { ActivityMemberState } from '../components/ActivityMember';
+import {
+  ActivityAnalytic,
+  ActivityMemberState,
+} from '../components/ActivityMember';
 
 export const convertData = (data: GetActivityMember[]) => {
   const result: ActivityMemberState[] = [];
-
   data.forEach((item) => {
     item.member.forEach((member) => {
       let memberObj = result.find((obj) => obj.id === `${member.id}`);
@@ -16,10 +18,41 @@ export const convertData = (data: GetActivityMember[]) => {
         };
         result.push(memberObj);
       }
-
       memberObj[item.id] = member.status;
     });
   });
-
   return result;
+};
+
+export const convertAnalyticData = (inputArray: GetActivityMember[]) => {
+  const statusCounts: { [key: string]: { [key: number]: number } } = {};
+  for (const day of inputArray) {
+    for (const member of day.member) {
+      const { status } = member;
+      if (!statusCounts[status]) {
+        statusCounts[status] = {};
+      }
+      statusCounts[status][day.id] = (statusCounts[status][day.id] || 0) + 1;
+    }
+  }
+  const fixedNames = ['ACCEPTED', 'REGISTERED', 'REJECTED', 'WITHDRAWN'];
+  const resultArray: ActivityAnalytic[] = fixedNames.map((name) => {
+    const statusObject: { name: string; [key: number]: number } = { name };
+    for (const day of inputArray) {
+      statusObject[day.id] = statusCounts[name]?.[day.id] || 0;
+    }
+    return statusObject;
+  });
+  const totalObject: { name: string; [key: number]: number } = {
+    name: 'TOTAL',
+  };
+  for (const day of inputArray) {
+    let total = 0;
+    for (const name of fixedNames) {
+      total += statusCounts[name]?.[day.id] || 0;
+    }
+    totalObject[day.id] = total;
+  }
+  resultArray.unshift(totalObject);
+  return resultArray;
 };
