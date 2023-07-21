9d2d90a82de6bbea6afc0a393264401f7a598282
Tan <ngoctana1k2014@gmail.com>
2023-07-22 05:45:55 +0700

    add activity status constant

---
+++ src/pages/Admin/Activity/constants/index.ts
@@ -0,0 +1,7 @@
+export const ActivityStatus: { [key: string]: string } = {
+  TOTAL: 'Tổng cộng',
+  ACCEPTED: 'Đã chấp nhận',
+  REGISTERED: 'Đăng ký',
+  REJECTED: 'Từ chối',
+  WITHDRAWN: 'Xin nghỉ',
+};
