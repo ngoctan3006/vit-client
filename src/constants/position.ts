8e0d39edea53a7c8b99970f6221337c5fb275880
Tan <ngoctana1k2014@gmail.com>
2023-07-22 13:54:20 +0700

    update role list

--- src/constants/position.ts
+++ src/constants/position.ts
@@ -11,34 +11,34 @@ export const Position = [
     label: 'Mảng trưởng Phong trào',
     value: 'TRUONG_PHONG_TRAO',
   },
-  {
-    label: 'Mảng phó Phong trào',
-    value: 'PHO_PHONG_TRAO',
-  },
+  // {
+  //   label: 'Mảng phó Phong trào',
+  //   value: 'PHO_PHONG_TRAO',
+  // },
   {
     label: 'Mảng trưởng Hành chính',
     value: 'TRUONG_HANH_CHINH',
   },
-  {
-    label: 'Mảng phó Hành chính',
-    value: 'PHO_HANH_CHINH',
-  },
+  // {
+  //   label: 'Mảng phó Hành chính',
+  //   value: 'PHO_HANH_CHINH',
+  // },
   {
     label: 'Mảng trưởng Hậu cần',
     value: 'TRUONG_HAU_CAN',
   },
-  {
-    label: 'Mảng phó Hậu cần',
-    value: 'PHO_HAU_CAN',
-  },
+  // {
+  //   label: 'Mảng phó Hậu cần',
+  //   value: 'PHO_HAU_CAN',
+  // },
   {
     label: 'Mảng trưởng Truyền thông',
     value: 'TRUONG_TRUYEN_THONG',
   },
-  {
-    label: 'Mảng phó Truyền thông',
-    value: 'PHO_TRUYEN_THONG',
-  },
+  // {
+  //   label: 'Mảng phó Truyền thông',
+  //   value: 'PHO_TRUYEN_THONG',
+  // },
   {
     label: 'Chủ nhiệm CLB VIT Media',
     value: 'CN_MEDIA',
