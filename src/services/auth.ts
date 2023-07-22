9d01a2c4470bae73cff19e896ac95c81dec0e5cd
Tan <ngoctana1k2014@gmail.com>
2023-07-22 17:15:08 +0700

    update data type

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -2,8 +2,8 @@ import { AxiosResponse } from 'axios';
 import { FirstLogin } from 'pages/Auth/FirstLogin';
 import { RequestResetPasswordState } from 'pages/Auth/ForgotPassword';
 import { ResetPasswordState } from 'pages/Auth/ResetPassword';
-import { API } from './axios';
 import { CreateUserDto } from 'src/pages/Admin/Member/types';
+import { API } from './axios';
 
 export const requestResetPasswordAPI = (
   data: RequestResetPasswordState
@@ -22,5 +22,15 @@ export const checkTokenAPI = (
 export const firstLogin = (data: FirstLogin): Promise<AxiosResponse<any>> =>
   API.put('/auth/first-login', data);
 
-export const signupUser = (data: CreateUserDto): Promise<AxiosResponse<any>> =>
+export const signupUser = (
+  data: CreateUserDto
+): Promise<AxiosResponse<{ data: { message: string } }>> =>
   API.post('/auth/signup', data);
+
+export const importMany = (
+  data: FormData,
+  isSendMail: boolean
+): Promise<AxiosResponse<{ data: { message: string } }>> =>
+  API.post('/auth/import-many', data, {
+    params: { isSendMail },
+  });
