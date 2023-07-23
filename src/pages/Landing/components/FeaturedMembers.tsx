04992ef123b883d3775b8c024ae8fe4f9f6d6020
ngoctan3006 <ngoctana1k2014@gmail.com>
2023-07-23 11:18:19 +0700

    Add contents for featured members

--- src/pages/Landing/components/FeaturedMembers.tsx
+++ src/pages/Landing/components/FeaturedMembers.tsx
@@ -23,10 +23,11 @@ const members: MemberInfo[] = [
         <h1 className="text-center">Humans of VIT</h1>
         <div className="relative"></div>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, earum.
-          Asperiores sapiente, obcaecati non facilis voluptates a et magni!
-          Consequatur, pariatur voluptatem. Nemo sunt dolorum beatae nam
-          possimus quae nihil.
+          Sau đây là những người anh, người chị, người bạn, vừa là những gương
+          mặt hết sức thân quen trong Đội, cũng đồng thời là những tấm gương
+          tình nguyện viên xuất sắc. Họ vừa là những người trụ cột trong gia
+          đình VIT, cũng đồng thời là những sinh viên có thành tích học tập và
+          hoạt động tốt. Hãy cùng xem họ là ai nhé.
         </p>
       </div>
     ),
@@ -36,40 +37,49 @@ const members: MemberInfo[] = [
     position: 'Lorem ipsum dolor sit amet.',
     description:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex earum vel sed placeat nihil, exercitationem, repudiandae ipsam alias praesentium sint odio quis ad nulla nobis excepturi. Facilis harum asperiores iste alias autem voluptatem reiciendis officia, quod sint, ipsum ullam iusto explicabo necessitatibus eius. Distinctio recusandae, hic error eius quo suscipit?',
-    image:
-      'https://i.pinimg.com/736x/55/8e/fe/558efe12b06a64e304ab04eab7bedba4.jpg',
+    image: 'https://drive.google.com/uc?id=1ddQiwDj7cqJaeU5pzq9iOO2KVOaGYnXd',
   },
   {
     name: 'Lorem, ipsum dolor.',
     position: 'Lorem ipsum dolor sit amet.',
     description:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex earum vel sed placeat nihil, exercitationem, repudiandae ipsam alias praesentium sint odio quis ad nulla nobis excepturi. Facilis harum asperiores iste alias autem voluptatem reiciendis officia, quod sint, ipsum ullam iusto explicabo necessitatibus eius. Distinctio recusandae, hic error eius quo suscipit?',
-    image:
-      'https://i.pinimg.com/originals/c5/14/26/c5142691e5d832d00166a721dbc43ae0.jpg',
+    image: 'https://drive.google.com/uc?id=1UsNWwE2F_w50HMLn4EPloHG0sVXdGrfl',
   },
   {
     name: 'Lorem, ipsum dolor.',
     position: 'Lorem ipsum dolor sit amet.',
     description:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex earum vel sed placeat nihil, exercitationem, repudiandae ipsam alias praesentium sint odio quis ad nulla nobis excepturi. Facilis harum asperiores iste alias autem voluptatem reiciendis officia, quod sint, ipsum ullam iusto explicabo necessitatibus eius. Distinctio recusandae, hic error eius quo suscipit?',
-    image:
-      'https://i.pinimg.com/474x/2d/f0/92/2df092a91d84d46215f288086b6509ad.jpg',
+    image: 'https://drive.google.com/uc?id=1iYmljzz9Fh_SiTa584Mkeo-7ZTxPcdp-',
   },
   {
     name: 'Lorem, ipsum dolor.',
     position: 'Lorem ipsum dolor sit amet.',
     description:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex earum vel sed placeat nihil, exercitationem, repudiandae ipsam alias praesentium sint odio quis ad nulla nobis excepturi. Facilis harum asperiores iste alias autem voluptatem reiciendis officia, quod sint, ipsum ullam iusto explicabo necessitatibus eius. Distinctio recusandae, hic error eius quo suscipit?',
-    image:
-      'https://i.pinimg.com/474x/2d/f0/92/2df092a91d84d46215f288086b6509ad.jpg',
+    image: 'https://drive.google.com/uc?id=1_ja9LGCrz7UBb_vwfzJr5ZrWtsl9CYcv',
   },
   {
     name: 'Lorem, ipsum dolor.',
     position: 'Lorem ipsum dolor sit amet.',
     description:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex earum vel sed placeat nihil, exercitationem, repudiandae ipsam alias praesentium sint odio quis ad nulla nobis excepturi. Facilis harum asperiores iste alias autem voluptatem reiciendis officia, quod sint, ipsum ullam iusto explicabo necessitatibus eius. Distinctio recusandae, hic error eius quo suscipit?',
-    image:
-      'https://i.pinimg.com/564x/b8/bb/f4/b8bbf4100e56894a54a152d8a122547d.jpg',
+    image: 'https://drive.google.com/uc?id=1aRCFgcU86kqvfYiOZiTvorYVhdZhmh1R',
+  },
+  {
+    name: 'Lorem, ipsum dolor.',
+    position: 'Lorem ipsum dolor sit amet.',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex earum vel sed placeat nihil, exercitationem, repudiandae ipsam alias praesentium sint odio quis ad nulla nobis excepturi. Facilis harum asperiores iste alias autem voluptatem reiciendis officia, quod sint, ipsum ullam iusto explicabo necessitatibus eius. Distinctio recusandae, hic error eius quo suscipit?',
+    image: 'https://drive.google.com/uc?id=1eqmYMNWySsba7cjs66FaKAFxZZAfW7c4',
+  },
+  {
+    name: 'Lorem, ipsum dolor.',
+    position: 'Lorem ipsum dolor sit amet.',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex earum vel sed placeat nihil, exercitationem, repudiandae ipsam alias praesentium sint odio quis ad nulla nobis excepturi. Facilis harum asperiores iste alias autem voluptatem reiciendis officia, quod sint, ipsum ullam iusto explicabo necessitatibus eius. Distinctio recusandae, hic error eius quo suscipit?',
+    image: 'https://drive.google.com/uc?id=1Vlml3GduiFq2x3O8EeXK3o6UQiIauoA1',
   },
 ];
 
@@ -80,16 +90,16 @@ const FeaturedMembers: React.FC<Props> = ({ className, id }) => {
         Cùng điểm qua những gương mặt tiêu biểu của Đội
       </h1>
       <HTMLFlipBook
-        width={700}
-        height={600}
+        width={500}
+        height={680}
         className="members-book"
         style={{}}
         startPage={1}
         size="fixed"
-        minWidth={700}
-        minHeight={600}
-        maxWidth={700}
-        maxHeight={600}
+        minWidth={500}
+        minHeight={680}
+        maxWidth={500}
+        maxHeight={680}
         drawShadow
         flippingTime={0.5}
         usePortrait
@@ -116,10 +126,6 @@ const FeaturedMembers: React.FC<Props> = ({ className, id }) => {
                     className="image"
                     style={{ backgroundImage: `url(${member.image})` }}
                   ></div>
-                  <div className="content">
-                    <h2 className="name text-center">{member.name}</h2>
-                    <p className="description">{member.description}</p>
-                  </div>
                 </div>
               </div>
             </div>
