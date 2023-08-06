2820f2e1dc0a0821d9692739b6dd1b506acd31ce
Tan <ngoctana1k2014@gmail.com>
2023-08-06 20:08:15 +0700

    update acitivity page

--- src/pages/Activity/index.tsx
+++ src/pages/Activity/index.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
-import './index.scss';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { getAllActivity } from 'redux/actions';
+import { activitySelector } from 'redux/slices/activity.slice';
+import { useAppDispatch } from 'redux/store';
+import { defaultQueryParam } from 'src/constants';
 import ActivityItem from './components/ActivityItem';
-import PaticipantsTable from './components/PaticipantsTable';
+import './index.scss';
 
 const Activity: React.FC = () => {
+  const dispatch = useAppDispatch();
+  const { activities } = useSelector(activitySelector);
+  const getActivities = async () => {
+    dispatch(getAllActivity(defaultQueryParam));
+  };
+
+  useEffect(() => {
+    getActivities();
+  }, []);
+
   return (
     <div
       className="activity m-5 d-flex "
@@ -11,7 +25,9 @@ const Activity: React.FC = () => {
         flexWrap: 'wrap',
       }}
     >
-      <PaticipantsTable />
+      {activities?.map((activity) => (
+        <ActivityItem key={activity.id} data={activity} />
+      ))}
     </div>
   );
 };
