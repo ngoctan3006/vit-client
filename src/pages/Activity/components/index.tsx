be8f094139862695fa3e7c53436ea9f5e56ef284
Tan <ngoctana1k2014@gmail.com>
2023-08-06 20:23:12 +0700

    add index export

---
+++ src/pages/Activity/components/index.tsx
@@ -0,0 +1,3 @@
+export { default as ActivityItem } from './ActivityItem';
+export { default as DetailActivity } from './DetailActivity';
+export { default as ParticipantsTable } from './ParticipantsTable';
