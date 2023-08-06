5275203d35389d54764ece0e3de6c97d5a3c1088
Tan <ngoctana1k2014@gmail.com>
2023-08-06 14:31:43 +0700

    fix hard code data feedback

--- src/pages/Feedback/index.tsx
+++ src/pages/Feedback/index.tsx
@@ -9,102 +9,105 @@ interface Feedback {
 
 const demoFeedbacks: Feedback[] = [
   {
-    date: '25//7/2023',
+    date: '25/07/2023',
+    content:
+      'Hoạt động đi dạy ngày hôm nay vui quá. mong là Đội sẽ có nhiều hoạt động giống như vậy hơn nữa ^^',
+    author: 'Giấu tên',
+  },
+  {
+    date: '25/07/2023',
+    content:
+      'Trong buổi họp Đội tuần trước đã xảy ra tình trạng mất trật tự, thành viên ngồi nói chuyện riêng không tập trung nghe, mong ban quản lý để ý và khắc phục để các buổi họp sau sẽ hiệu quả hơn',
+    author: 'Giấu tên',
+  },
+  {
+    date: '23/07/2023',
+    content:
+      'Các bạn ban quản lý thật tuyệt vời, mong Đội sẽ luôn phát triển nhé ^^',
+    author: 'Giấu tên',
+  },
+  {
+    date: '20/07/2023',
     content: 'Đội trưởng hôi nách quá',
     author: 'Giấu tên',
   },
   {
-    date: '25//7/2023',
+    date: '25/07/2023',
     content: 'Đội trưởng hôi nách quá',
     author: 'Giấu tên',
   },
   {
-    date: '25//7/2023',
+    date: '25/07/2023',
     content: 'Đội trưởng hôi nách quá',
     author: 'Giấu tên',
   },
   {
-    date: '25//7/2023',
+    date: '25/07/2023',
     content: 'Đội trưởng hôi nách quá',
     author: 'Giấu tên',
   },
   {
-    date: '25//7/2023',
+    date: '25/07/2023',
     content: 'Đội trưởng hôi nách quá',
     author: 'Giấu tên',
   },
   {
-    date: '25//7/2023',
+    date: '25/07/2023',
     content: 'Đội trưởng hôi nách quá',
     author: 'Giấu tên',
   },
   {
-    date: '25//7/2023',
+    date: '25/07/2023',
     content: 'Đội trưởng hôi nách quá',
     author: 'Giấu tên',
   },
   {
-    date: '25//7/2023',
+    date: '25/07/2023',
     content: 'Đội trưởng hôi nách quá',
     author: 'Giấu tên',
   },
   {
-    date: '25//7/2023',
+    date: '25/07/2023',
     content: 'Đội trưởng hôi nách quá',
     author: 'Giấu tên',
   },
   {
-    date: '25//7/2023',
+    date: '25/07/2023',
     content: 'Đội trưởng hôi nách quá',
     author: 'Giấu tên',
   },
   {
-    date: '25//7/2023',
+    date: '25/07/2023',
     content: 'Đội trưởng hôi nách quá',
     author: 'Giấu tên',
   },
   {
-    date: '25//7/2023',
+    date: '25/07/2023',
     content: 'Đội trưởng hôi nách quá',
     author: 'Giấu tên',
   },
   {
-    date: '25//7/2023',
+    date: '25/07/2023',
     content: 'Đội trưởng hôi nách quá',
     author: 'Giấu tên',
   },
   {
-    date: '25//7/2023',
+    date: '25/07/2023',
     content: 'Đội trưởng hôi nách quá',
     author: 'Giấu tên',
   },
   {
-    date: '25//7/2023',
+    date: '25/07/2023',
     content: 'Đội trưởng hôi nách quá',
     author: 'Giấu tên',
   },
   {
-    date: '25//7/2023',
+    date: '25/07/2023',
     content: 'Đội trưởng hôi nách quá',
     author: 'Giấu tên',
   },
   {
-    date: '25//7/2023',
-    content: 'Đội trưởng hôi nách quá',
-    author: 'Giấu tên',
-  },
-  {
-    date: '25//7/2023',
-    content: 'Đội trưởng hôi nách quá',
-    author: 'Giấu tên',
-  },
-  {
-    date: '25//7/2023',
-    content: 'Đội trưởng hôi nách quá',
-    author: 'Giấu tên',
-  },
-  {
-    date: '25//7/2023',
+    date: '25/07/2023',
     content: 'Đội trưởng hôi nách quá',
     author: 'Giấu tên',
   },
