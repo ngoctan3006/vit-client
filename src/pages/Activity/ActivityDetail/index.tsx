5c663031b9bd21571757d3376af03e0f0c498173
Tan <ngoctana1k2014@gmail.com>
2023-08-09 00:20:57 +0700

    update UI

--- src/pages/Activity/ActivityDetail/index.tsx
+++ src/pages/Activity/ActivityDetail/index.tsx
@@ -1,15 +1,18 @@
-import { Table } from 'antd';
+import { Button, Col, Row, Table, Typography } from 'antd';
 import type { ColumnsType } from 'antd/es/table';
 import { Loading } from 'components';
 import moment from 'moment';
 import React, { useEffect } from 'react';
 import { AiOutlineFieldTime } from 'react-icons/ai';
+import { MdOutlineLocationOn } from 'react-icons/md';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { getActivity } from 'redux/actions';
 import { ActivityTime, activitySelector } from 'redux/slices/activity.slice';
 import { useAppDispatch } from 'redux/store';
 import { DATE_FORMAT, TIME_FORMAT } from 'src/constants';
+import { getColorOfDate } from 'src/utils';
+import './index.scss';
 
 const ActivityDetail: React.FC = () => {
   const { id } = useParams();
@@ -28,21 +31,30 @@ const ActivityDetail: React.FC = () => {
     {
       title: 'Ngày diễn ra',
       key: 'date_time',
-      render: (_: string, { start_time }: ActivityTime) =>
-        moment(start_time).format(DATE_FORMAT),
+      render: (_: string, { start_time, end_time }: ActivityTime) => (
+        <Typography.Text type={getColorOfDate(start_time, end_time)}>
+          {moment(start_time).format(DATE_FORMAT)}
+        </Typography.Text>
+      ),
     },
     {
-      title: 'Thời gian kết thúc',
+      title: 'Thời gian',
       key: 'hour_time',
-      render: (_: string, { start_time, end_time }: ActivityTime) =>
-        `${moment(start_time).format(TIME_FORMAT)} - ${moment(end_time).format(
-          TIME_FORMAT
-        )}`,
+      render: (_: string, { start_time, end_time }: ActivityTime) => (
+        <Typography.Text type={getColorOfDate(start_time, end_time)}>
+          {moment(start_time).format(TIME_FORMAT)} -{' '}
+          {moment(end_time).format(TIME_FORMAT)}
+        </Typography.Text>
+      ),
     },
     {
       title: 'Đăng ký',
       key: 'action',
-      render: () => <a>Đăng ký</a>,
+      render: () => (
+        <Button disabled={moment().isAfter(activity?.deadline)} type="primary">
+          {moment().isAfter(activity?.deadline) ? 'Đã hết hạn' : 'Đăng ký'}
+        </Button>
+      ),
     },
   ];
 
@@ -58,51 +70,59 @@ const ActivityDetail: React.FC = () => {
   if (loading) return <Loading />;
 
   return (
-    <div className="mt-20 pt-10 w-full d-flex">
-      <div
-        className="d-flex p-5 ml-5 mb-5"
-        style={{
-          width: '22%',
-          backgroundColor: '#d5edf1',
-          border: '0px ',
-          borderRadius: '10px',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}
-      >
-        <div>
-          <img src="https://ctsv.hust.edu.vn/static/img/activity.a80f3233.png" />
-        </div>
-        <div style={{ textAlign: 'center', margin: '10px', fontWeight: '600' }}>
-          {activity?.name}
-        </div>
-        <div
-          style={{
-            backgroundColor: 'white',
-            padding: '20px',
-            borderRadius: '10px',
-          }}
-        >
-          <div style={{ marginBottom: '10px', fontWeight: '500' }}>
-            {activity?.description}
+    <div className="activity-detail p-5">
+      <Row gutter={16}>
+        <Col span={5}>
+          <div className="activity-left d-flex flex-column align-center p-3">
+            <div>
+              <img src="https://ctsv.hust.edu.vn/static/img/activity.a80f3233.png" />
+            </div>
+            <Typography.Title className="text-center" level={3}>
+              {activity?.name}
+            </Typography.Title>
+            <div className="activity-info w-full p-5">
+              <Typography.Title level={5}>
+                {activity?.description}
+              </Typography.Title>
+              <Typography.Title
+                type={
+                  moment().isBefore(activity?.deadline) ? 'success' : 'danger'
+                }
+                title={
+                  moment().isBefore(activity?.deadline)
+                    ? 'Deadline đăng ký'
+                    : 'Đã hết thời gian đăng ký'
+                }
+                level={5}
+                className="d-flex align-center gap-2"
+              >
+                <AiOutlineFieldTime />
+                {`${moment(activity?.deadline).format(DATE_FORMAT)} - ${moment(
+                  activity?.deadline
+                ).format(TIME_FORMAT)}`}
+              </Typography.Title>
+              <Typography.Title
+                level={5}
+                title="Địa điểm diễn ra hoạt động"
+                className="d-flex align-center gap-2 mt-0"
+              >
+                <MdOutlineLocationOn />
+                {activity?.location}
+              </Typography.Title>
+            </div>
           </div>
-          <div style={{ marginBottom: '10px', fontWeight: '450' }}>
-            <AiOutlineFieldTime /> {activity?.deadline.slice(0, 10)}
-          </div>
-        </div>
-      </div>
-      <div className="pa-5 mx-5 " style={{ width: '75%' }}>
-        <h2>Nội dung</h2>
-        <div className="my-5" style={{ color: 'red' }}>
-          {activity?.description}
-        </div>
-        <h2>Danh sách các kíp đăng ký:</h2>
-        <Table
-          columns={columns}
-          dataSource={activity?.times}
-          pagination={false}
-        />
-      </div>
+        </Col>
+        <Col span={19}>
+          <Typography.Title level={4}>
+            Danh sách các kíp đăng ký:
+          </Typography.Title>
+          <Table
+            columns={columns}
+            dataSource={activity?.times}
+            pagination={false}
+          />
+        </Col>
+      </Row>
     </div>
   );
 };
