a90c36a91b30764a12ac740f221d01611cca9d74
Tan <ngoctana1k2014@gmail.com>
2023-08-12 04:04:43 +0700

    get manage UI

--- src/pages/Home/components/LeftSide.tsx
+++ src/pages/Home/components/LeftSide.tsx
@@ -1,9 +1,15 @@
-import { List } from 'antd';
-import React from 'react';
+import { Avatar, List, message } from 'antd';
+import React, { useEffect, useState } from 'react';
 import { BsBook, BsFileEarmarkRuled, BsMusicNote } from 'react-icons/bs';
+import { Link } from 'react-router-dom';
+import { Loading } from 'src/components';
+import { UserManagement, getUserManagement } from 'src/services/user';
 
 const LeftSide: React.FC = () => {
-  const data = [
+  const [management, setManagement] = useState<UserManagement[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  const noiQuyData = [
     {
       key: 'rules',
       title: 'Nội quy',
@@ -24,37 +30,78 @@ const LeftSide: React.FC = () => {
     },
   ];
 
+  const getManagement = async () => {
+    try {
+      setLoading(true);
+      const { data } = await getUserManagement();
+      setManagement(data.data);
+    } catch (error: any) {
+      message.error(error.response.data.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  console.log(management);
+
+  useEffect(() => {
+    getManagement();
+  }, []);
+
   return (
-    <div className="left-side p-5">
-      <div className="mb-5">
-        <h3 className="subtitle mb-2">Ban quản lý</h3>
-        <hr />
-        <p>MVP Phạm Hoàng Hiệp</p>
-        <p>MVP Phạm Hoàng Hiệp</p>
-        <p>MVP Phạm Hoàng Hiệp</p>
-        <p>MVP Phạm Hoàng Hiệp</p>
+    <>
+      <div className="left-side p-5">
+        <div className="mb-5">
+          <h3 className="subtitle mb-2">Ban quản lý</h3>
+          <hr />
+          <List
+            itemLayout="horizontal"
+            dataSource={management}
+            renderItem={(item) => (
+              <List.Item>
+                <List.Item.Meta
+                  avatar={
+                    <Avatar src={item?.avatar}>
+                      {item.username.charAt(0).toUpperCase()}
+                    </Avatar>
+                  }
+                  title={
+                    <Link className="text-16" to={`profile/${item.id}`}>
+                      {item.fullname}
+                    </Link>
+                  }
+                  description={
+                    item.position === 'DOI_TRUONG' ? 'Đội trưởng' : 'Đội phó'
+                  }
+                />
+              </List.Item>
+            )}
+          />
+        </div>
+        <div className="mb-5">
+          <h3 className="subtitle mb-2">Nội quy - Cương lĩnh</h3>
+          <hr />
+          <List
+            itemLayout="horizontal"
+            dataSource={noiQuyData}
+            renderItem={(item) => (
+              <List.Item>
+                <List.Item.Meta
+                  avatar={item.icon}
+                  title={
+                    <a className="text-16" href={item.link} target="_blank">
+                      {item.title}
+                    </a>
+                  }
+                />
+              </List.Item>
+            )}
+          />
+        </div>
       </div>
-      <div className="mb-5">
-        <h3 className="subtitle mb-2">Nội quy - Cương lĩnh</h3>
-        <hr />
-        <List
-          itemLayout="horizontal"
-          dataSource={data}
-          renderItem={(item) => (
-            <List.Item>
-              <List.Item.Meta
-                avatar={item.icon}
-                title={
-                  <a className="text-16" href={item.link} target="_blank">
-                    {item.title}
-                  </a>
-                }
-              />
-            </List.Item>
-          )}
-        />
-      </div>
-    </div>
+
+      {loading && <Loading />}
+    </>
   );
 };
 
