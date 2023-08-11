e4f0c9a535b22aa15d58e1f68e77eacff6e7b59a
Tan <ngoctana1k2014@gmail.com>
2023-08-12 02:58:17 +0700

    add top event

--- src/pages/Home/components/Main.tsx
+++ src/pages/Home/components/Main.tsx
@@ -1,5 +1,6 @@
 import { Carousel } from 'antd';
 import MHX from 'assets/images/home/MHX.png';
+import NHSVTN from 'assets/images/home/NHSVTN.jpg';
 import TTNV from 'assets/images/home/TTNV.jpg';
 import XYT from 'assets/images/home/XYT.jpg';
 import React from 'react';
@@ -12,9 +13,10 @@ const Main: React.FC = () => {
         <div className="d-center">
           <div className="card">
             <Carousel autoplay>
-              <img src={MHX} alt="mhx" />
-              <img src={XYT} alt="mhx" />
-              <img src={TTNV} alt="mhx" />
+              <img src={MHX} alt="mùa hè xanh" />
+              <img src={NHSVTN} alt="ngày hội sinh viên tình nguyện" />
+              <img src={XYT} alt="xuân yêu thương" />
+              <img src={TTNV} alt="tuyển tình nguyện viên" />
             </Carousel>
           </div>
         </div>
