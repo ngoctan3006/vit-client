db6b3e09ae0a10f2106afc5b68bc5cea5cef5b1c
Tan <ngoctana1k2014@gmail.com>
2023-08-12 03:36:23 +0700

    add noi quy, cuong linh

--- src/pages/Home/components/LeftSide.tsx
+++ src/pages/Home/components/LeftSide.tsx
@@ -1,10 +1,33 @@
+import { List } from 'antd';
 import React from 'react';
+import { BsBook, BsFileEarmarkRuled, BsMusicNote } from 'react-icons/bs';
 
 const LeftSide: React.FC = () => {
+  const data = [
+    {
+      key: 'rules',
+      title: 'Nội quy',
+      link: 'https://drive.google.com/file/d/1eF2i3qi8-MW_QlnWz4GVSaibhUFKsg1A/view?usp=drive_link',
+      icon: <BsFileEarmarkRuled />,
+    },
+    {
+      key: 'credo',
+      title: 'Cương lĩnh',
+      link: 'https://drive.google.com/file/d/1vlUMr5_EOHJvCFE97axx_B3jh9aObi7s/view?usp=drive_link',
+      icon: <BsBook />,
+    },
+    {
+      key: 'songs',
+      title: 'Bài hát truyền thống',
+      link: 'https://drive.google.com/file/d/1Vu62T1bqa6_34_Q0FOhVEspSPHuAFHNQ/view?usp=drive_link',
+      icon: <BsMusicNote />,
+    },
+  ];
+
   return (
     <div className="left-side p-5">
       <div className="mb-5">
-        <h3 className="subtitle mb-2">Ghế nóng</h3>
+        <h3 className="subtitle mb-2">Ban quản lý</h3>
         <hr />
         <p>MVP Phạm Hoàng Hiệp</p>
         <p>MVP Phạm Hoàng Hiệp</p>
@@ -12,20 +35,24 @@ const LeftSide: React.FC = () => {
         <p>MVP Phạm Hoàng Hiệp</p>
       </div>
       <div className="mb-5">
-        <h3 className="subtitle mb-2">Tài liệu</h3>
+        <h3 className="subtitle mb-2">Nội quy - Cương lĩnh</h3>
         <hr />
-        <p>Chuyên ngành</p>
-        <p>Chuyên ngành</p>
-        <p>Chuyên ngành</p>
-        <p>Chuyên ngành</p>
-      </div>
-      <div className="mb-5">
-        <h3 className="subtitle mb-2">Group</h3>
-        <hr />
-        <p>Mùa hè xanh</p>
-        <p>Mùa hè xanh</p>
-        <p>Mùa hè xanh</p>
-        <p>Mùa hè xanh</p>
+        <List
+          itemLayout="horizontal"
+          dataSource={data}
+          renderItem={(item) => (
+            <List.Item>
+              <List.Item.Meta
+                avatar={item.icon}
+                title={
+                  <a className="text-16" href={item.link} target="_blank">
+                    {item.title}
+                  </a>
+                }
+              />
+            </List.Item>
+          )}
+        />
       </div>
     </div>
   );
