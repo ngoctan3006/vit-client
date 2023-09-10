4b6f3448616d9a25658a2a6c174e4b652b5c7796
namln2k <namln2aug2k@gmail.com>
2023-09-10 13:21:09 +0700

    Fix bug css

--- src/pages/Landing/components/Header/Mobile/index.tsx
+++ src/pages/Landing/components/Header/Mobile/index.tsx
@@ -26,7 +26,7 @@ const LandingHeaderDesktop: React.FC = () => {
             xmlns="http://www.w3.org/2000/svg"
             width="36"
             height="36"
-            viewBox="0 0 36 36"
+            viewBox="0 0 24 24"
           >
             <path d="M4 6h16v2H4zm0 5h16v2H4zm0 5h16v2H4z"></path>
           </svg>
@@ -46,7 +46,7 @@ const LandingHeaderDesktop: React.FC = () => {
               xmlns="http://www.w3.org/2000/svg"
               width="36"
               height="36"
-              viewBox="0 0 36 36"
+              viewBox="0 0 24 24"
             >
               <path d="m16.192 6.344-4.243 4.242-4.242-4.242-1.414 1.414L10.535 12l-4.242 4.242 1.414 1.414 4.242-4.242 4.243 4.242 1.414-1.414L13.364 12l4.242-4.242z"></path>
             </svg>
